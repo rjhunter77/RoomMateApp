9256f6792f6a625142f6efaf751752981bceb27b
Ryan Hunter <rjhunter77@gmail.com>
2014-11-06 20:15:15 -0700

    big push that includes two of the three pages with working controllers and data-binding

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,3 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
@@ -22,13 +15,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
     // setup an abstract state for the tabs directive
     .state('tab', {
       url: "/tab",
@@ -36,7 +23,39 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       templateUrl: "templates/tabs.html"
     })
 
-    // Each tab has its own nav history stack:
+
+    .state('tab.settings', {
+        url: '/settings',
+         views: {
+          'tab-settings': {
+            templateUrl: 'templates/settings.html',
+            controller: 'SettingsCtrl'
+          }
+        }
+    })
+
+
+    .state('tab.history', {
+      url: '/history',
+      views: {
+        'tab-history': {
+          templateUrl: 'templates/tab-history.html',
+          controller: 'HistoryCtrl'
+        }
+      }
+    })
+
+
+    // .state('tab.textbook-detail', {
+    //   url: '/textbook/:textbookId',
+    //   views: {
+    //     'tab-dash': {
+    //       templateUrl: 'templates/textbook-detail.html',
+    //       controller: 'TextbookDetailCtrl'
+    //     }
+    //   }
+    // })
+
     .state('tab.new', {
       url: '/new',
       views: {
@@ -45,41 +64,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
           controller: 'NewCtrl'
         }
       }
-    })
-
-  .state('tab.dash', {
-      url: '/dash',
-      views: {
-          'tab-dash': {
-              templateUrl: 'templates/tab-dash.html',
-              controller: 'DashCtrl'
-          }
-      }
-  })
-
-
-    .state('tab.textbook-detail', {
-      url: '/textbook/:textbookId',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/textbook-detail.html',
-          controller: 'TextbookDetailCtrl'
-        }
-      }
-    })
-
-    .state('tab.account', {
-      url: '/account',
-      views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
-        }
-      }
-    })
+    });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/settings');
 
 });
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,54 @@
-angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $http) {
-  $http.get('data/book_list.json').success(function(data){
-      $scope.books = data;
+angular.module('starter.controllers', [])
+.controller('SettingsCtrl', function($scope, $http) {
+  $http.get('data/roomMates.json').success(function(data){
+      $scope.roomMates = data;
   });
 
-  $scope.orderProp = 'title';
+  $scope.remove = function(roommate){
+    $scope.roomMates.splice( $scope.roomMates.indexOf(roommate), 1 );
+  };
 
+  $scope.add = function(name){
+    $scope.roomMates.push({'name': name});
+  };
 
 })
+
+.controller('HistoryCtrl', function($scope, $http) {
+  $http.get('data/roomMates.json').success(function(data){
+      $scope.roomMates = data;
+  });
+
+  $http.get('data/transactions.json').success(function(data){
+      $scope.transactions = data;
+  });
+
+  $scope.getName = function(id){
+    for(var i=0; i < $scope.roomMates.length; i++){
+      if($scope.roomMates[i].id == id){
+       return $scope.roomMates[i].name;
+      }
+    }
+    return;
+  };
+
+
+
+
+  // var init = function(){
+  //   for(i=0; i <= $scope.transactions.length; i++){
+  //     console.log($scope.transactions[i].who);
+  //     // $scope.transactions[i].who = $scope.roomMates[$scope.transactions[i].who].name
+  //   }
+  // };
+
+  // init()
+  
+
+})
+
+
 
 .controller('NewCtrl', function($scope, Textbooks) {
   $scope.textbooks = Textbooks.all();
@@ -48,35 +88,33 @@ angular.module('starter.controllers', [])
       tb.condition.description = $scope.conditions[tb.condition.value -1].description;
       console.log("TB", tb);
   };
-})
-
-.controller('TextbookDetailCtrl', function($http, $scope, $stateParams, Textbooks) {
-  // var textbooks = Textbooks.all();
-  // console.log(textbooks[1]);
-  // $scope.textbook = textbooks[2];
-  $http.get('data/book_list.json').success(function(data){
-      var books = data;
-      for (var key in books) {
-        if (books.hasOwnProperty(key)) {
-          if ($stateParams.textbookId == books[key]['id']) {
-          	console.log(books[key]);
-          	$scope.textbook = books[key];
-          }
-        }
-      }
-  });
-  $http.get('data/user_list.json').success(function(data){
-      var user = data;
-      for (var key in user) {
-        if (user.hasOwnProperty(key)) {
-          if ($scope.textbook['sellerID'] == user[key]['sellerID']) {
-          	console.log(user[key]);
-          	$scope.seller = user[key];
-          }
-        }
-      }
-  });
-})
-
-.controller('AccountCtrl', function($scope) {
 });
+
+// .controller('TextbookDetailCtrl', function($http, $scope, $stateParams, Textbooks) {
+//   // var textbooks = Textbooks.all();
+//   // console.log(textbooks[1]);
+//   // $scope.textbook = textbooks[2];
+//   $http.get('data/book_list.json').success(function(data){
+//       var books = data;
+//       for (var key in books) {
+//         if (books.hasOwnProperty(key)) {
+//           if ($stateParams.textbookId == books[key]['id']) {
+//           	console.log(books[key]);
+//           	$scope.textbook = books[key];
+//           }
+//         }
+//       }
+//   });
+//   $http.get('data/user_list.json').success(function(data){
+//       var user = data;
+//       for (var key in user) {
+//         if (user.hasOwnProperty(key)) {
+//           if ($scope.textbook['sellerID'] == user[key]['sellerID']) {
+//           	console.log(user[key]);
+//           	$scope.seller = user[key];
+//           }
+//         }
+//       }
+//   });
+// })
+
